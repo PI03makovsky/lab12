21ba25b6051e6e8a95af09234ae385fc37721b57
PI03makovsky <91346352+PI03makovsky@users.noreply.github.com>
2022-01-19 03:23:27 +0700

    11lb

---
+++ Program11.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace шарпик8 {
+
+static void Main(string[] args)
+
+{
+    date[,] date2 = new date[3, 2];
+
+    date[] date1 = new date[2];
+
+    for (int i = 0; i < 1; i++)
+
+    {
+
+        date1[i] = new date(12, 11, 2021);
+
+        date1[i].print();
+
+    }
+
+    for (int i = 0; i < 3; i++)
+
+    {
+
+        for (int j = 0; j < 2; j++)
+
+        {
+
+            date2[i, j] = new date(12 + i, 11, 2021);
+
+            date2[i, j].print();
+
+            Console.Write(" ");
+
+        }
+
+    }
+}
+
+}
+
+
+
